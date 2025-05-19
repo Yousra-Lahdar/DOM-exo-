889e6bbdd78bcd775e4cad9b43f676d24f2a1756
yousra <yousra.italiana@gmail.com>
2025-05-19 15:52:06 +0200

    new exo3

--- chap1/chap1.js
+++ chap1/chap1.js
@@ -1,3 +1,13 @@
-const id = prompt("Quel est ton nom ?");
-const titleElement = document.querySelector('.title');
-titleElement.innerHTML = `Hello, ${id}`;
+// fichier script.js
+const bearsPictures = [
+    "https://placebear.com/500/279",
+    "https://placebear.com/500/280",
+    "https://placebear.com/500/300",
+    "https://placebear.com/500/302",
+    "https://placebear.com/500/305",
+  ];
+  // À toi de jouer ici 👇 
+  const bear = document.querySelectorAll('img')
+  for(let i=0;i<bear.length;i++ ){
+    bear[i].src = bearsPictures[i];
+  }
