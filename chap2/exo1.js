fd898e6d075f64c72b226d031598d0a176bbe1e5
yousra <yousra.italiana@gmail.com>
2025-05-19 16:21:57 +0200

    exo1

---
+++ chap2/exo1.js
@@ -0,0 +1,6 @@
+
+const chatTwo = document.createElement('img');
+chatTwo.src = "https://placecats.com/408/287";
+chatTwo.alt = "chat";
+const div = document.querySelector('.second-card')
+div.appendChild(chatTwo)
