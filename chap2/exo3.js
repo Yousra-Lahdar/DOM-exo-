3c86fedbf15b12f0ef5fe02d4ffb4a4a90c96801
yousra <yousra.italiana@gmail.com>
2025-05-19 16:54:54 +0200

    exo 3

---
+++ chap2/exo3.js
@@ -0,0 +1,7 @@
+const box = document.querySelector('.box');
+
+box.style.width = '400px';
+box.style.height = '300px';
+box.style.backgroundColor = 'yellow';
+box.style.color = 'lightblue';
+box.style.fontSize = '60px';
