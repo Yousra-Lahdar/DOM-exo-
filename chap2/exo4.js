561f594af1ff65d04084d62e8b8f2c987abe4ae6
yousra <yousra.italiana@gmail.com>
2025-05-20 09:55:50 +0200

    new exo4,5

---
+++ chap2/exo4.js
@@ -0,0 +1,11 @@
+
+const container = document.querySelector('.container-boxes');
+
+
+const newBox = document.createElement('div');
+
+
+newBox.classList.add('box');
+
+
+container.appendChild(newBox);
